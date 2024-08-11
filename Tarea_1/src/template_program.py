fda498197a12156cff9bd5d15ff2c2fcf736d946
carlosgglez <carlosgg@lcg.unam.mx>
2024-08-10 18:55:36 -0600

    Se lleno con la información correcta el archivo template_program

--- Tarea_1/src/template_program.py
+++ Tarea_1/src/template_program.py
@@ -1,29 +1,36 @@
 '''
 NAME
-       
+        Tarea 1
 
 VERSION
-        
+        1
 
 AUTHOR
-        
+        Carlos García González
 
 DESCRIPTION
+        Este script de Python utiliza las bibliotecas de "Biopython" y de
+        "Entrez" para hacer una consulta en la base de datos de "Pubmed", 
+        regresando la siguiente información de todos los FieldList:
+            - Name
+            - Full Name
+            - Description
         
+        De igual manera, regresa la descripción del primer FieldList
 
 CATEGORY
         
 
 USAGE
 
-    % python programName [-parameter] [parameter_value]
+    python template_program
     
 
 ARGUMENTS
 
 
 METHOD
-
+    Biopython/Tarea_1
 
 SEE ALSO
 
@@ -36,39 +43,31 @@ SEE ALSO
 # =                            imports
 # ===========================================================================
 
-
-
-
-
-# ===========================================================================
-# =                            Command Line Options
-# ===========================================================================
-
-
-
-
-
-
-# ===========================================================================
-# =                            functions
-# ===========================================================================
-
-
-
+from Bio import Entrez
+from pprint import pprint  # Mejor visualización de diccionarios
 
 
 # ===========================================================================
 # =                            main
 # ===========================================================================
 
+Entrez.email = "carlosgg@lcg.unam.mx"
 
-# step 1.
+handle = Entrez.einfo(db="pubmed")
+record = Entrez.read(handle)
 
+# 1. Iterar sobre todos los campos en FieldList e imprimir nombre, FullName y descripción
+for field in record["DbInfo"]["FieldList"]:
+    print(field["Name"] + "," + field["FullName"] + "," + field["Description"])
 
-# step 2.
+# Imprimir la URL de la consulta
+print("\nURL de la consulta:", handle.url)
 
+# 2. Imprimir la descripción del primer campo
+primer_campo = record['DbInfo']['FieldList'][0]
+print("\nDescripción del primer campo:")
+print(primer_campo["Description"])
 
-# step 3.
-
-
+# Cerrar el handle después de obtener toda la información
+handle.close()
 
