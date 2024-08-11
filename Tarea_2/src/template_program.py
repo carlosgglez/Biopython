2803409d9b1c6f571b6f5e9a32daeccc8c808af0
carlosgglez <carlosgg@lcg.unam.mx>
2024-08-10 22:59:49 -0600

    Se actualizo la información del archivo template_program

--- Tarea_2/src/template_program.py
+++ Tarea_2/src/template_program.py
@@ -13,6 +13,12 @@ DESCRIPTION
         "Entrez" para hacer una consulta en la base de datos de "Protein", 
         regresando la descripción del FieldList "ECNO" y la descripción del
         LinklList "protein_protein_small_genome".
+        
+        La otra función que tiene el prgrama es de buscar en la base de 
+        datos de Pubmed las entradas que hay de la Doctora Constance Auvynet,
+        especificando encontrar los artículos que en el titulo mencionen algo
+        sobre "peptide", "antimicrobial" o "migration".
+
 
 CATEGORY
         
