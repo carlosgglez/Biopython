d2677022fe1efd271057e28fc82b560a8387f85d
carlosgglez <160920668+carlosgglez@users.noreply.github.com>
2024-08-26 22:20:50 -0600

    Update template_program.py

--- Tarea_5/src/template_program.py
+++ Tarea_5/src/template_program.py
@@ -9,7 +9,16 @@ AUTHOR
         Carlos García González
 
 DESCRIPTION
-        
+         Este script de Python tiene dos funciones, utilizando las bibliotecas
+         de "Biopython" y el módulo "Entrez" para interactuar con la base de 
+         datos "Protein" del NCBI. La primera función realiza una búsqueda para
+         encontrar proteínas relacionadas al gen DEFA en el mosquiro Aedes aegypti
+         e imprime en pantalla los IDs que coinciden con el término de la búsqueda.
+
+         La segunda función selecciona el primer ID y realiza una búsqueda en 
+         "GeneBank" e imprime información detallada acerca de esta.
+
+
 
 CATEGORY
         
