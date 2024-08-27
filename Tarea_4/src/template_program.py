a75ea65a9370cd97956cde760f38326eb940ed4b
carlosgglez <160920668+carlosgglez@users.noreply.github.com>
2024-08-26 21:17:54 -0600

    Update template_program.py

--- Tarea_4/src/template_program.py
+++ Tarea_4/src/template_program.py
@@ -9,7 +9,17 @@ AUTHOR
         Carlos García González
 
 DESCRIPTION
-        
+        Este script de Python realiza tres funciones utilizando la biblioteca de
+        “Biopython” y “Entrez” para interactuar con la base de datos de PubMed. 
+        La primera función  busca artículos de la Dra. Constance Auvynete, utilizando 
+        ciertas palabras clave del título, límita la busqueda a 100 resultados e 
+        imprime en pantalla los IDs de los artículos que coinciden con el término de 
+        la búsqueda. 
+        La segunda función recupera los abstracts (resúmenes) y los 
+        guarda en un archivo. 
+        La tercera y última función  busca artículos en PubMed 
+        que citen a los artículos encontrados en la búsqueda inicial, los IDs de los 
+        artículos citantes se extraen y se almacenan en un archivo.  
 
 CATEGORY
         
@@ -23,7 +33,7 @@ ARGUMENTS
 
 
 METHOD
-    Biopython/Tarea_2
+    Biopython/Tarea_4
 
 SEE ALSO
 
